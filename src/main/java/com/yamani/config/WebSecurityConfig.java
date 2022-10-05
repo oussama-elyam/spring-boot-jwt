4adf161c4123f680ce8f35dab75717f81349d2d5
elyamart <elyamanioussama3@gmail.com>
2022-10-05 17:17:05 +0100

    Allow the url /register to be allowed without applying spring security-

--- src/main/java/com/yamani/config/WebSecurityConfig.java
+++ src/main/java/com/yamani/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().
+				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
